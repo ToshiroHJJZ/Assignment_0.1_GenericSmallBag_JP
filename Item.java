2e2f0019946dce2edd56b923c601c2c9d0a4d484
Jonathan Penaloza <121989712+ToshiroHJJZ@users.noreply.github.com>
2023-08-31 15:17:08 -0400

    Add files via upload

---
+++ Item.java
@@ -0,0 +1,13 @@
+
+public class Item <T>{
+	 // T stands for "Type"
+	private T e;
+	
+	public void setE(T e) {
+		this.e = e;
+	}
+	
+	public T getE() {
+		return e;
+	}
+}
---
+++ SmallBag.java
@@ -0,0 +1,12 @@
+
+public class SmallBag<T> {
+	private T Item;
+
+	public T getItem() {
+		return Item;
+	}
+
+	public void setItem(T Item) {
+		this.Item = Item;
+	}
+}
